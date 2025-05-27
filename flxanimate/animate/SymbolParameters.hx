c5bc388e49d6f801210af5ee4003a284d47b02cd
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-05-27 20:36:42 -0300

    Fix SymbolParameters & FlxLayer trying to destroy null bitmaps

--- flxanimate/animate/SymbolParameters.hx
+++ flxanimate/animate/SymbolParameters.hx
@@ -161,9 +161,9 @@ class SymbolParameters implements IFilterable
 		_filterFrame = FlxDestroyUtil.destroy(_filterFrame);
 		_filterCamera = FlxDestroyUtil.destroy(_filterCamera);
 		_filterMatrix = null;
-		FlxG.bitmap.remove(FlxG.bitmap.get(FlxG.bitmap.findKeyForBitmap(_bmp1)));
+		if (_bmp1 != null) FlxG.bitmap.remove(FlxG.bitmap.get(FlxG.bitmap.findKeyForBitmap(_bmp1)));
 		_bmp1 = FlxDestroyUtil.dispose(_bmp1);
-		FlxG.bitmap.remove(FlxG.bitmap.get(FlxG.bitmap.findKeyForBitmap(_bmp2)));
+		if (_bmp2 != null) FlxG.bitmap.remove(FlxG.bitmap.get(FlxG.bitmap.findKeyForBitmap(_bmp2)));
 		_bmp2 = FlxDestroyUtil.dispose(_bmp2);
 	}
 
